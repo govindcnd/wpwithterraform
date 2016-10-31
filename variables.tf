d5ded102980370c58f9f19c97195446bd23c803b
root <root@logserver.rockettv.localdomain>
2016-10-31 07:31:49 +0000

    elb + instance

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+# ubuntu-trusty-14.04 (x64)
+variable "aws_amis" {
+  default = {
+    "us-east-1" = "ami-5f709f34"
+    "us-west-2" = "ami-7f675e4f"
+  }
+}
+
+variable "availability_zones" {
+  default     = "us-east-1b,us-east-1c,us-east-1d,us-east-1e"
+  description = "List of availability zones "
+}
+
+variable "key_name" {
+  description = "punch in the pem key"
+}
+
+variable "aws_access_key" {
+  description = "punch in the access key"
+}
+
+variable "aws_secret_key" {
+  description = "punch in the secret key"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "AWS instance type"
+}
+
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "1"
+}
