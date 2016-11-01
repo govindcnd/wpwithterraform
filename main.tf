0c9100ae1c87d4b9cf4bf11043720e9fb0e2b576
root <root@logserver.rockettv.localdomain>
2016-11-01 11:12:29 +0000

    subnet fix for elb

--- main.tf
+++ main.tf
@@ -133,27 +133,6 @@ resource "aws_internet_gateway" "default" {
     vpc_id = "${aws_vpc.smvpc.id}"
 }
 
-resource "aws_subnet" "us-east-1c-public" {
-    vpc_id = "${aws_vpc.smvpc.id}"
-
-    cidr_block = "${var.public1_subnet_cidr}"
-    availability_zone = "us-east-1c"
-}
-
-resource "aws_subnet" "us-east-1b-public" {
-    vpc_id = "${aws_vpc.smvpc.id}"
-
-    cidr_block = "${var.public2_subnet_cidr}"
-    availability_zone = "us-east-1b"
-}
-
-resource "aws_subnet" "us-east-1d-public" {
-    vpc_id = "${aws_vpc.smvpc.id}"
-
-    cidr_block = "${var.public3_subnet_cidr}"
-    availability_zone = "us-east-1d"
-}
-
 resource "aws_route_table" "us-east-1-public" {
     vpc_id = "${aws_vpc.smvpc.id}"
 
@@ -164,21 +143,14 @@ resource "aws_route_table" "us-east-1-public" {
 }
 
 resource "aws_route_table_association" "us-east-1c-public" {
-    subnet_id = "${aws_subnet.us-east-1c-public.id}"
+    subnet_id = "${aws_subnet.subnet_1.id}"
     route_table_id = "${aws_route_table.us-east-1-public.id}"
 }
 
 resource "aws_route_table_association" "us-east-1b-public" {
-    subnet_id = "${aws_subnet.us-east-1b-public.id}"
+    subnet_id = "${aws_subnet.subnet_2.id}"
     route_table_id = "${aws_route_table.us-east-1-public.id}"
 }
-
-
-resource "aws_route_table_association" "us-east-1d-public" {
-    subnet_id = "${aws_subnet.us-east-1d-public.id}"
-    route_table_id = "${aws_route_table.us-east-1-public.id}"
-}
-
 
 
 resource "aws_db_instance" "default" {
