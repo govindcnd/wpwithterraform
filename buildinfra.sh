9db9c086aa133906bfadedfb8c4d9cc95b200eb5
root <root@logserver.rockettv.localdomain>
2016-11-02 12:21:56 +0000

    building script

--- buildinfra.sh
+++ buildinfra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 terraform apply -state=$4terraform.tfstate  -var "asg_min=$1" -var "aws_access_key=$2" -var "aws_secret_key=$3" -var "env-name=$4" -var "instance_type=$5" -var "key_name=$6"
-
+echo " now the infrastructure is deployed  once the instance starts listering try accessing the dns name as shown in the output"
