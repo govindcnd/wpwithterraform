3c518e9b91e71394dc5e95acd7e1a0ab7695632a
root <root@logserver.rockettv.localdomain>
2016-11-02 13:51:19 +0000

    update on how to

--- buildinfra.sh
+++ buildinfra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 terraform apply -state=$4terraform.tfstate  -var "asg_min=$1" -var "aws_access_key=$2" -var "aws_secret_key=$3" -var "env-name=$4" -var "instance_type=$5" -var "key_name=$6"
-echo " now the infrastructure is deployed  once the instance starts listering try accessing the dns name as shown in the output"
+echo " now the infrastructure is deployed  once the instance starts listening on ELB try accessing the dns name as shown in the output ( usually takes 2-5 minutes for ansible to build the image and instance to be listening on ELB"
