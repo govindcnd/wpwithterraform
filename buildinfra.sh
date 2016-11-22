e2b72f1a495f5267478dc46b6cafc9adde0bad26
root <root@logserver.rockettv.localdomain>
2016-11-22 09:49:07 +0000

    throttle and check status of deployment

--- buildinfra.sh
+++ buildinfra.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-terraform apply -state=$4terraform.tfstate  -var "asg_min=$1" -var "aws_access_key=$2" -var "aws_secret_key=$3" -var "env-name=$4" -var "instance_type=$5" -var "key_name=$6"
-echo " now the infrastructure is deployed  once the instance starts listening on ELB try accessing the dns name as shown in the output ( usually takes 2-5 minutes for ansible to build the image and instance to be listening on ELB"
+terraform apply -state=$4terraform.tfstate  -var "asg_min=$1" -var "aws_access_key=$2" -var "aws_secret_key=$3" -var "env-name=$4" -var "instance_type=$5" -var "key_name=$6" | tee file1.txt
+dname=$(cat file1.txt | grep 'elb_dns_name' |  awk '{print $3}')
+echo $dname
+echo "to be completed soon"
+domain=http://${dname}
+path='/wp-admin/install.php'
+base_url="$domain$path"
+a=4
+while [ $a -ge 0 ] ; do
+sts=$(curl -s -o /dev/null --globoff -w "%{http_code}" -I  "$base_url")
+if [ "$sts" = "200" ]; then
+    a=0
+    break
+  else
+    sleep 5
+    echo -n '.'
+  fi
+done
+echo "ELB END POINT NAME IS $dname"
--- subnets.tf
+++ subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "subnet_1" {
   vpc_id            = "${aws_vpc.smvpc.id}"
   cidr_block        = "${var.subnet_1_cidr}"
   availability_zone = "${var.az_1}"
+  map_public_ip_on_launch = "true"
 
   tags {
     Name = "main_subnet1"
